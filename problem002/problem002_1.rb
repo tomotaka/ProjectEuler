275eb4155ac18fc814eea03610442cdcd2b7dd51
Tomotaka Ito <tomotaka.ito@gmail.com>
2011-03-09 01:01:00 +0900

    - Problem 002 is solved with Ruby.

---
+++ problem002/problem002_1.rb
@@ -0,0 +1,20 @@
+MAX = 400_000 - 1
+
+def fib(max)
+  before_n = 1
+  n = 1
+  yield n
+  while n < max do
+    tmp = n
+    n += before_n
+    before_n = tmp
+    yield n
+  end
+end
+
+even_total = 0
+fib(MAX) do |n|
+  even_total += n if n % 2 == 0
+end
+
+p even_total
